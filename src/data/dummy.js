6dac32db358e4e0f17840fceb9c4d6f1036fd99a
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-02-03 07:55:36 +0530

    completed the dummy data structure

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -1,42 +1,178 @@
-const dateWiseInvestmentTrans = [
+export const dateWiseInvestmentTrans = [
   {
-    date: "2023-01-31T14:16:00.000Z",
+    date: "2023-01-31",
     amount: 4650,
   },
   {
-    date: "2023-01-30T14:16:00.000Z",
+    date: "2023-01-30",
     amount: 6451,
   },
   {
-    date: "2023-01-29T14:16:00.000Z",
+    date: "2023-01-29",
     amount: 5942,
   },
   {
-    date: "2023-01-28T14:16:00.000Z",
+    date: "2023-01-28",
     amount: 4972,
   },
   {
-    date: "2023-01-27T14:16:00.000Z",
+    date: "2023-01-27",
     amount: 5540,
   },
   {
-    date: "2023-01-26T14:16:00.000Z",
+    date: "2023-01-26",
     amount: 7940,
   },
   {
-    date: "2023-01-25T14:16:00.000Z",
+    date: "2023-01-25",
     amount: 9854,
   },
   {
-    date: "2023-01-24T14:16:00.000Z",
+    date: "2023-01-24",
     amount: 6840,
   },
   {
-    date: "2023-01-23T14:16:00.000Z",
+    date: "2023-01-23",
     amount: 3641,
   },
   {
-    date: "2023-01-22T14:16:00.000Z",
+    date: "2023-01-22",
     amount: 8541,
   },
 ];
+
+export const dateWiseSaleTrans = [
+  {
+    date: "2023-01-31",
+    amount: 5046,
+  },
+  {
+    date: "2023-01-30",
+    amount: 5164,
+  },
+  {
+    date: "2023-01-29",
+    amount: 4259,
+  },
+  {
+    date: "2023-01-28",
+    amount: 7249,
+  },
+  {
+    date: "2023-01-27",
+    amount: 4055,
+  },
+  {
+    date: "2023-01-26",
+    amount: 4079,
+  },
+  {
+    date: "2023-01-25",
+    amount: 5498,
+  },
+  {
+    date: "2023-01-24",
+    amount: 4068,
+  },
+  {
+    date: "2023-01-23",
+    amount: 4136,
+  },
+  {
+    date: "2023-01-22",
+    amount: 4185,
+  },
+];
+
+export const investmentTransForADay = [
+  {
+    date: "2023-01-22",
+    trans: [
+      {
+        id: "#4d62hj5",
+        item: "pita bread",
+        amount: "300",
+      },
+      {
+        id: "#4d65dg4",
+        item: "eggs",
+        amount: "80",
+      },
+      {
+        id: "#4d6245gh",
+        item: "petrol",
+        amount: "180",
+      },
+      {
+        id: "#4d645j5",
+        item: "milk",
+        amount: "50",
+      },
+      {
+        id: "#4d686gs",
+        item: "rent",
+        amount: "3000",
+      },
+      {
+        id: "#4d68534",
+        item: "chicken",
+        amount: "1300",
+      },
+      {
+        id: "#4d64534",
+        item: "paalak",
+        amount: "30",
+      },
+      {
+        id: "#4d6877d",
+        item: "spices",
+        amount: "120",
+      },
+    ],
+  },
+];
+
+// probably in a modal
+export const detailOfAInvestmentTrans = {
+  id: "#4d6877d",
+  item: "spices",
+  amount: "120",
+  method: "cash",
+  receipt: "some image of bill",
+};
+
+export const saleTransForADay = {
+  date: "2023-01-28",
+  trans: [
+    {
+      id: "#9865gfd",
+      item: "khabooz",
+      unit: 15,
+      amount: 1335,
+    },
+    {
+      id: "#9865ggd",
+      item: "chai",
+      unit: 21,
+      amount: 630,
+    },
+    {
+      id: "#9865g6d",
+      item: "water",
+      unit: 10,
+      amount: 200,
+    },
+    {
+      id: "#9865g2d",
+      item: "rumali wrap",
+      unit: 7,
+      amount: 480,
+    },
+    {
+      id: "#9865ghd",
+      item: "pita bread shawarma",
+      unit: 25,
+      amount: 2000,
+    },
+  ],
+};
