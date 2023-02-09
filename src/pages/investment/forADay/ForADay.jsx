14c6daa15a1df0aa98eadde167380da233a4f284
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-02-09 23:15:28 +0530

    created styled header cell for the table in prev commit | refactored forADay page comp of both investment and sale to use newly created table comp

--- src/pages/investment/forADay/ForADay.jsx
+++ src/pages/investment/forADay/ForADay.jsx
@@ -1,51 +1,17 @@
-import { useState } from "react";
-
-import {
-  Box,
-  Typography,
-  TableContainer,
-  Paper,
-  TableRow,
-  TableHead,
-  Table,
-  TableCell,
-  tableCellClasses,
-  TableBody,
-  TablePagination,
-} from "@mui/material";
-
-import { styled } from "@mui/material/styles";
+import { Box, Typography } from "@mui/material";
 
 import dayjs from "dayjs";
 
-const StyledTableCell = styled(TableCell)(() => ({
-  [`&.${tableCellClasses.head}`]: {
-    fontWeight: "bold",
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
+import { ForADayTable } from "../../../components/forADayTable";
 
 import { FlexBox } from "../../../layouts/flexBox";
 
 import { investmentTransForADay } from "../../../data/dummy";
 
+import { useParams } from "react-router-dom";
+
 function ForADay() {
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
-
-  function handleChangePage(event, newPage) {
-    // We can use this situation to fetch another batch of data
-    // console.log(dateWiseInvestmentTrans.length / rowsPerPage > newPage);
-    setPage(newPage);
-  }
-
-  function handleChangeRowsPerPage(event) {
-    console.log(event.target.value);
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  }
+  const { date } = useParams();
 
   return (
     <FlexBox
@@ -65,7 +31,7 @@ function ForADay() {
       >
         {/* Header */}
         <FlexBox
-          sx={{
+          csx={{
             justifyContent: "flex-end",
             gap: 10,
           }}
@@ -81,48 +47,15 @@ function ForADay() {
               Date
             </Typography>
             <Typography variant="subtitle2" component={"p"}>
-              {dayjs(investmentTransForADay.date).format("D MMM YYYY")}
+              {dayjs(date).format("D MMM YYYY")}
             </Typography>
           </FlexBox>
         </FlexBox>
         {/* BOdy */}
-        <Box sx={{ width: "100%" }}>
-          <TableContainer
-            component={Paper}
-            sx={{ height: "auto", width: "100%" }}
-          >
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <StyledTableCell>ID</StyledTableCell>
-                  <StyledTableCell>Item</StyledTableCell>
-                  <StyledTableCell>Amount (INR)</StyledTableCell>
-                </TableRow>
-              </TableHead>
-
-              <TableBody>
-                {investmentTransForADay.trans
-                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                  .map((inv) => (
-                    <TableRow key={inv.id} hover sx={{ cursor: "pointer" }}>
-                      <TableCell>{inv.id}</TableCell>
-                      <TableCell>{inv.item}</TableCell>
-                      <TableCell>{inv.amount}</TableCell>
-                    </TableRow>
-                  ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[5, 10]}
-            component="div"
-            count={investmentTransForADay.trans.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-        </Box>
+        <ForADayTable
+          data={investmentTransForADay}
+          tableHeads={["ID", "Item", "Amount (INR)"]}
+        />
       </Box>
     </FlexBox>
   );
--- src/pages/sale/forADay/ForADay.jsx
+++ src/pages/sale/forADay/ForADay.jsx
@@ -1,50 +1,16 @@
-import { useState } from "react";
-
-import {
-  Box,
-  Typography,
-  TableContainer,
-  Paper,
-  TableRow,
-  TableHead,
-  Table,
-  TableCell,
-  tableCellClasses,
-  TableBody,
-  TablePagination,
-} from "@mui/material";
-
-import { styled } from "@mui/material/styles";
+import { Box, Typography } from "@mui/material";
 
 import dayjs from "dayjs";
 
-const StyledTableCell = styled(TableCell)(() => ({
-  [`&.${tableCellClasses.head}`]: {
-    fontWeight: "bold",
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
+import { useParams } from "react-router-dom";
 
 import { saleTransForADay } from "../../../data/dummy";
 import { FlexBox } from "../../../layouts/flexBox";
 
+import { ForADayTable } from "../../../components/forADayTable";
+
 function ForADay() {
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
-
-  function handleChangePage(event, newPage) {
-    // We can use this situation to fetch another batch of data
-    // console.log(dateWiseInvestmentTrans.length / rowsPerPage > newPage);
-    setPage(newPage);
-  }
-
-  function handleChangeRowsPerPage(event) {
-    console.log(event.target.value);
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  }
+  const { date } = useParams();
 
   return (
     <FlexBox
@@ -80,50 +46,15 @@ function ForADay() {
               Date
             </Typography>
             <Typography variant="subtitle2" component={"p"}>
-              {dayjs(saleTransForADay.date).format("D MMM YYYY")}
+              {dayjs(date).format("D MMM YYYY")}
             </Typography>
           </FlexBox>
         </FlexBox>
         {/* BOdy */}
-        <Box sx={{ width: "100%" }}>
-          <TableContainer
-            component={Paper}
-            sx={{ height: "auto", width: "100%" }}
-          >
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <StyledTableCell>ID</StyledTableCell>
-                  <StyledTableCell>Item</StyledTableCell>
-                  <StyledTableCell>Unit</StyledTableCell>
-                  <StyledTableCell>Amount (INR)</StyledTableCell>
-                </TableRow>
-              </TableHead>
-
-              <TableBody>
-                {saleTransForADay.trans
-                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                  .map((inv) => (
-                    <TableRow key={inv.id} hover sx={{ cursor: "pointer" }}>
-                      <TableCell>{inv.id}</TableCell>
-                      <TableCell>{inv.item}</TableCell>
-                      <TableCell>{inv.unit}</TableCell>
-                      <TableCell>{inv.amount}</TableCell>
-                    </TableRow>
-                  ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[5, 10]}
-            component="div"
-            count={saleTransForADay.trans.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-        </Box>
+        <ForADayTable
+          data={saleTransForADay}
+          tableHeads={["ID", "Item", "Unit", "Amount (INR)"]}
+        />
       </Box>
     </FlexBox>
   );
