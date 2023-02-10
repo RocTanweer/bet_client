a661881f1ca522eaf4ab36e52ba9127cddbc6055
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-02-10 08:17:30 +0530

    created lib file for react-router-dom

---
+++ src/lib/reactRouterDom/router.jsx
@@ -0,0 +1,39 @@
+import App from "../../App";
+import { createBrowserRouter } from "react-router-dom";
+import { Dashboard } from "../../pages/dashboard";
+import { InvestmentHistory } from "../../pages/investment/history";
+import { SaleHistory } from "../../pages/sale/history";
+import { ForADay as InvForADay } from "../../pages/investment/forADay";
+import { ForADay as SalForADay } from "../../pages/sale/forADay";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+
+    children: [
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "investment/history",
+        element: <InvestmentHistory />,
+      },
+      {
+        path: "investment/history/:date",
+        element: <InvForADay />,
+      },
+      {
+        path: "sale/history",
+        element: <SaleHistory />,
+      },
+      {
+        path: "sale/history/:date",
+        element: <SalForADay />,
+      },
+    ],
+  },
+]);
+
+export default router;
