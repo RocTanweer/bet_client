ba2fdb0719809f0eca8658deb7c8593a82cc9537
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-02-10 08:26:10 +0530

    modified main comp to use MyRouterProvider comp

--- src/main.jsx
+++ src/main.jsx
@@ -1,53 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
 import { CssBaseline } from "@mui/material";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Dashboard } from "./pages/dashboard";
-import { InvestmentHistory } from "./pages/investment/history";
-import { SaleHistory } from "./pages/sale/history";
-import { ForADay as InvForADay } from "./pages/investment/forADay";
-import { ForADay as SalForADay } from "./pages/sale/forADay";
-
-async function saleForADayLoader({ params }) {
-  return { params };
-}
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-
-    children: [
-      {
-        path: "dashboard",
-        element: <Dashboard />,
-      },
-      {
-        path: "investment/history",
-        element: <InvestmentHistory />,
-      },
-      {
-        path: "investment/history/:date",
-        element: <InvForADay />,
-      },
-      {
-        path: "sale/history",
-        element: <SaleHistory />,
-      },
-      {
-        path: "sale/history/:date",
-        element: <SalForADay />,
-        loader: saleForADayLoader,
-      },
-    ],
-  },
-]);
+import { MyRouterProvider } from "./context/reactRouterDom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CssBaseline />
-    <RouterProvider router={router} />
+    <MyRouterProvider />
   </React.StrictMode>
 );
