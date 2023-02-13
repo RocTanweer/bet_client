b689e8fcdf02df9fea54737662898399824317aa
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-02-13 18:15:59 +0530

    applied formik and yup validation on investment add form

--- src/pages/investment/add/Add.jsx
+++ src/pages/investment/add/Add.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import {
   Box,
   Typography,
@@ -11,15 +9,61 @@ import {
   InputLabel,
   FormControl,
   Button,
+  FormHelperText,
 } from "@mui/material";
+
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 import FileUploadIcon from "@mui/icons-material/FileUpload";
 
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 import { FlexBox } from "../../../layouts/flexBox";
-import { DateChooser } from "../../../components/dateChooser";
+
+const FILE_SIZE = 262144000;
+const SUPPORTED_FORMATS = ["image/jpg", "image/jpeg", "image/png"];
+
+const validationSchema = Yup.object({
+  investmentItem: Yup.string("Choose an investment item").required(
+    "This is required!"
+  ),
+  amount: Yup.number("Enter amount invested").required("This is required"),
+  payment: Yup.string("Choose payment method").required("This is required"),
+  date: Yup.date()
+    .nullable()
+    .typeError("date is required")
+    .required("Date is required"),
+  receiptImg: Yup.mixed()
+    .test(
+      "fileSize",
+      "File too large",
+      (value) => value === null || value.size <= FILE_SIZE
+    )
+    .test(
+      "fileFormat",
+      "Unsupported file type",
+      (value) => value === null || SUPPORTED_FORMATS.includes(value.type)
+    )
+    .notRequired(),
+});
 
 function Add() {
-  const [date, setDate] = useState(null);
+  const formik = useFormik({
+    initialValues: {
+      investmentItem: "",
+      amount: "",
+      payment: "",
+      date: null,
+      receiptImg: null,
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
 
   return (
     <FlexBox
@@ -43,49 +87,113 @@ function Add() {
         </Box>
         {/* Body */}
         <Box>
-          <Stack gap={3} component={"form"}>
-            <Autocomplete
-              freeSolo
-              options={["one", "two", "three"]}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="Investment Item"
-                  name="investmentItem"
-                  id="investmentItem"
-                />
-              )}
-            />
+          <Stack gap={3} component={"form"} onSubmit={formik.handleSubmit}>
+            <Box>
+              <Autocomplete
+                name="investmentItem"
+                freeSolo
+                options={["one", "two", "three"]}
+                onChange={(e, value) => {
+                  formik.setFieldValue("investmentItem", value);
+                }}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Investment Item"
+                    id="investmentItem"
+                    name="investmentItem"
+                    error={
+                      formik.errors.investmentItem &&
+                      formik.touched.investmentItem
+                    }
+                  />
+                )}
+              />
+              <FormHelperText
+                error={
+                  formik.errors.investmentItem && formik.touched.investmentItem
+                }
+              >
+                {formik.errors.investmentItem &&
+                  formik.touched.investmentItem &&
+                  formik.errors.investmentItem}
+              </FormHelperText>
+            </Box>
 
-            <TextField
-              label="Amount"
-              type="number"
-              name="amount"
-              id="amount"
-              fullWidth
-            />
+            <Box>
+              <TextField
+                label="Amount"
+                type="number"
+                name="amount"
+                id="amount"
+                fullWidth
+                onChange={formik.handleChange}
+                error={formik.errors.amount && formik.touched.amount}
+              />
+              <FormHelperText
+                error={formik.errors.amount && formik.touched.amount}
+              >
+                {formik.errors.amount &&
+                  formik.touched.amount &&
+                  formik.errors.amount}
+              </FormHelperText>
+            </Box>
 
             <FormControl sx={{ width: "100%" }}>
-              <InputLabel id="payment-label">Payment</InputLabel>
+              <InputLabel
+                error={formik.errors.payment && formik.touched.payment}
+                id="payment-label"
+              >
+                Payment
+              </InputLabel>
               <Select
                 labelId="payment-label"
                 name="payment"
                 id="payment"
                 label="Payment"
                 fullWidth
+                value={formik.values.payment}
+                onChange={formik.handleChange}
+                error={formik.errors.payment && formik.touched.payment}
               >
                 <MenuItem value={"online"}>Online</MenuItem>
                 <MenuItem value={"offline"}>Offline</MenuItem>
-              </Select>
+              </Select>{" "}
+              <FormHelperText
+                error={formik.errors.payment && formik.touched.payment}
+              >
+                {formik.errors.payment &&
+                  formik.touched.payment &&
+                  formik.errors.payment}
+              </FormHelperText>
             </FormControl>
 
-            <DateChooser
-              label={"Date"}
-              date={date}
-              setDate={setDate}
-              block={true}
-              csx={{ width: "100%" }}
-            />
+            <Box>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="date"
+                  value={formik.values.date}
+                  onChange={(newValue) => {
+                    formik.setFieldValue("date", newValue);
+                  }}
+                  id="date"
+                  name="date"
+                  renderInput={(params) => (
+                    <TextField
+                      sx={{ width: "100%" }}
+                      {...params}
+                      error={formik.errors.date && formik.touched.date}
+                    />
+                  )}
+                />
+              </LocalizationProvider>
+
+              <FormHelperText error={formik.errors.date && formik.touched.date}>
+                {formik.errors.date &&
+                  formik.touched.date &&
+                  formik.errors.date}
+              </FormHelperText>
+            </Box>
 
             <label htmlFor="receiptImg">
               <input
@@ -94,6 +202,9 @@ function Add() {
                 id="receiptImg"
                 name="receiptImg"
                 style={{ display: "none" }}
+                onChange={(e) => {
+                  formik.setFieldValue("receiptImg", e.target.files[0]);
+                }}
               />
               <Button
                 component="span"
@@ -104,8 +215,10 @@ function Add() {
               >
                 Upload Receipt
               </Button>
+              <FormHelperText variant="h7">
+                {formik.values.receiptImg && formik.values.receiptImg.name}
+              </FormHelperText>
             </label>
-
             <Button variant="contained" fullWidth type="submit">
               Submit
             </Button>
