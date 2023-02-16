35d5070f10dc50fd37524582daf3c909506a0cce
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-02-16 22:37:20 +0530

    modified DateChooser comp to be applied in any form with formik handling

--- src/components/dateChooser/DateChooser.jsx
+++ src/components/dateChooser/DateChooser.jsx
@@ -5,17 +5,24 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
-function DateChooser({ label, date, setDate, csx }) {
+function DateChooser({ label, formik, name, csx, size }) {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DatePicker
         label={label}
-        value={date}
+        name={name}
+        id={name}
+        value={formik.values.date}
         onChange={(newValue) => {
-          setDate(dayjs(newValue));
+          formik.setFieldValue("date", newValue);
         }}
         renderInput={(params) => (
-          <TextField sx={{ ...csx }} size="small" {...params} />
+          <TextField
+            sx={{ ...csx }}
+            size={size && size}
+            {...params}
+            error={formik.errors.date && formik.touched.date}
+          />
         )}
       />
     </LocalizationProvider>
