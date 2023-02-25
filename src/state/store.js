8dbf8974cbe7bf7dd009f6b603a9ba98608f863b
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-02-25 14:36:47 +0530

    ADD: rootReducer and initialState logic for the whole app using combineReducers idea from redux

---
+++ src/state/store.js
@@ -0,0 +1,42 @@
+import {
+  userRegisterReducer,
+  userLoginReducer,
+  userLoginOAuthReducer,
+  userLogoutReducer,
+  userDetailsReducer,
+  userDetailsUpdateReducer,
+} from "./reducers/userReducers";
+
+/**
+ * i This function is same as combineReducer from redux
+ * @param {Object} slices An Object containing all the reducers in a key-value pair
+ * @returns rootReducer which is a usual reducer but it remembers slices object as closure and operate upon it and not switch-case stuff
+ */
+const combineReducers = (slices) => {
+  return (state, action) => {
+    return Object.keys(slices).reduce((accu, prop) => {
+      return {
+        ...accu,
+        [prop]: slices[prop](accu[prop], action),
+      };
+    }, state);
+  };
+};
+
+export const rootReducer = combineReducers({
+  userRegister: userRegisterReducer,
+  userLogin: userLoginReducer,
+  userLoginOAuth: userLoginOAuthReducer,
+  userDetails: userDetailsReducer,
+  userLogout: userLogoutReducer,
+  userDetailsUpdate: userDetailsUpdateReducer,
+});
+
+export const initialState = {
+  userRegister: {},
+  userLogin: {},
+  userLoginOAuth: {},
+  userDetails: {},
+  userLogout: {},
+  userDetailsUpdate: {},
+};
