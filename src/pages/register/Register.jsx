1e237b45427e90cf1b5edc4ce983c15025ab3342
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-02-26 20:41:12 +0530

    MOD: Register page component to make sure logged in user wont come to this page (protected this route from logged in users)

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -8,15 +8,23 @@ import {
   FormHelperText,
 } from "@mui/material";
 
-import GoogleIcon from "@mui/icons-material/Google";
-
 import { FlexBox } from "../../layouts/flexBox";
-import { NavLink } from "react-router-dom";
+import { Navigate, NavLink } from "react-router-dom";
+
+import { useGlobalState } from "../../context/globalState";
 
 import { useFormik } from "formik";
 import { registerFormValSch } from "../../lib/yup/validationSchemas";
 
+import GoogleOAuthBtn from "../../components/googleOAuthBtn/GoogleOAuthBtn";
+
 function Register() {
+  const { state } = useGlobalState();
+
+  const {
+    userDetails: { loginToken },
+  } = state;
+
   const formik = useFormik({
     initialValues: {
       firstName: "",
@@ -29,115 +37,119 @@ function Register() {
   });
 
   return (
-    <FlexBox csx={{ minHeight: "100vh" }}>
-      <Box sx={{ maxWidth: "530px", width: "100%", height: "auto" }}>
-        <Box sx={{ mb: 3 }}>
-          <Typography variant="h4">Create an account</Typography>
-        </Box>
+    <>
+      {loginToken && <Navigate to={"/dashboard"} replace={true} />}
 
-        <Stack component={"form"} spacing={2} onSubmit={formik.handleSubmit}>
-          <FlexBox csx={{ gap: 1 }}>
-            <Box sx={{ flex: 1 }}>
-              <TextField
-                label={"First Name"}
-                type={"text"}
-                name={"firstName"}
-                id={"firstName"}
-                fullWidth
-                onChange={formik.handleChange}
-                error={formik.errors.firstName && formik.touched.firstName}
-              />
-              <FormHelperText
-                error={formik.errors.firstName && formik.touched.firstName}
-              >
-                {formik.errors.firstName &&
-                  formik.touched.firstName &&
-                  formik.errors.firstName}
-              </FormHelperText>
-            </Box>
-
-            <Box sx={{ flex: 1 }}>
-              <TextField
-                label={"Last Name"}
-                type={"text"}
-                name={"lastName"}
-                id={"lastName"}
-                fullWidth
-                onChange={formik.handleChange}
-                error={formik.errors.lastName && formik.touched.lastName}
-              />
-              <FormHelperText
-                error={formik.errors.lastName && formik.touched.lastName}
-              >
-                {formik.errors.lastName &&
-                  formik.touched.lastName &&
-                  formik.errors.lastName}
-              </FormHelperText>
-            </Box>
-          </FlexBox>
-
-          <Box>
-            <TextField
-              label={"Email"}
-              type={"text"}
-              name={"email"}
-              id={"email"}
-              fullWidth
-              onChange={formik.handleChange}
-              error={formik.errors.email && formik.touched.email}
-            />
-            <FormHelperText error={formik.errors.email && formik.touched.email}>
-              {formik.errors.email &&
-                formik.touched.email &&
-                formik.errors.email}
-            </FormHelperText>
+      <FlexBox csx={{ minHeight: "100vh" }}>
+        <Box sx={{ maxWidth: "530px", width: "100%", height: "auto" }}>
+          <Box sx={{ mb: 3 }}>
+            <Typography variant="h4">Create an account</Typography>
           </Box>
 
-          <Box>
-            <TextField
-              label={"Password"}
-              type={"password"}
-              name={"password"}
-              id={"password"}
-              fullWidth
-              onChange={formik.handleChange}
-              error={formik.errors.password && formik.touched.password}
-            />
-            <FormHelperText
-              error={formik.errors.password && formik.touched.password}
+          <Stack component={"form"} spacing={2} onSubmit={formik.handleSubmit}>
+            <FlexBox csx={{ gap: 1 }}>
+              <Box sx={{ flex: 1 }}>
+                <TextField
+                  label={"First Name"}
+                  type={"text"}
+                  name={"firstName"}
+                  id={"firstName"}
+                  fullWidth
+                  onChange={formik.handleChange}
+                  error={formik.errors.firstName && formik.touched.firstName}
+                />
+                <FormHelperText
+                  error={formik.errors.firstName && formik.touched.firstName}
+                >
+                  {formik.errors.firstName &&
+                    formik.touched.firstName &&
+                    formik.errors.firstName}
+                </FormHelperText>
+              </Box>
+
+              <Box sx={{ flex: 1 }}>
+                <TextField
+                  label={"Last Name"}
+                  type={"text"}
+                  name={"lastName"}
+                  id={"lastName"}
+                  fullWidth
+                  onChange={formik.handleChange}
+                  error={formik.errors.lastName && formik.touched.lastName}
+                />
+                <FormHelperText
+                  error={formik.errors.lastName && formik.touched.lastName}
+                >
+                  {formik.errors.lastName &&
+                    formik.touched.lastName &&
+                    formik.errors.lastName}
+                </FormHelperText>
+              </Box>
+            </FlexBox>
+
+            <Box>
+              <TextField
+                label={"Email"}
+                type={"text"}
+                name={"email"}
+                id={"email"}
+                fullWidth
+                onChange={formik.handleChange}
+                error={formik.errors.email && formik.touched.email}
+              />
+              <FormHelperText
+                error={formik.errors.email && formik.touched.email}
+              >
+                {formik.errors.email &&
+                  formik.touched.email &&
+                  formik.errors.email}
+              </FormHelperText>
+            </Box>
+
+            <Box>
+              <TextField
+                label={"Password"}
+                type={"password"}
+                name={"password"}
+                id={"password"}
+                fullWidth
+                onChange={formik.handleChange}
+                error={formik.errors.password && formik.touched.password}
+              />
+              <FormHelperText
+                error={formik.errors.password && formik.touched.password}
+              >
+                {formik.errors.password &&
+                  formik.touched.password &&
+                  formik.errors.password}
+              </FormHelperText>
+            </Box>
+
+            <Button type="submit" variant="contained" fullWidth>
+              Register
+            </Button>
+
+            <FlexBox csx={{ gap: 1, justifyContent: "flex-start" }}>
+              <Typography variant="subtitle2">Already registerd?</Typography>
+              <Typography component={NavLink} to={"/login"} replace={true}>
+                login here
+              </Typography>
+            </FlexBox>
+
+            <Divider
+              orientation="horizontal"
+              variant="subtitle2"
+              flexItem
+              sx={{ color: "text.secondary" }}
             >
-              {formik.errors.password &&
-                formik.touched.password &&
-                formik.errors.password}
-            </FormHelperText>
-          </Box>
+              Or
+            </Divider>
 
-          <Button type="submit" variant="contained" fullWidth>
-            Register
-          </Button>
-
-          <FlexBox csx={{ gap: 1, justifyContent: "flex-start" }}>
-            <Typography variant="subtitle2">Already registerd?</Typography>
-            <Typography component={NavLink} to={"/login"}>
-              login here
-            </Typography>
-          </FlexBox>
-
-          <Divider
-            orientation="horizontal"
-            variant="subtitle2"
-            flexItem
-            sx={{ color: "text.secondary" }}
-          >
-            Or
-          </Divider>
-
-          <Button type="button" variant="outlined" startIcon={<GoogleIcon />}>
-            Login with Google
-          </Button>
-        </Stack>
-      </Box>
-    </FlexBox>
+            <GoogleOAuthBtn />
+          </Stack>
+        </Box>
+      </FlexBox>
+    </>
   );
 }
 
