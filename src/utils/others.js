6badcf307a91953310dff6592e6af4493ce6f856
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-02-28 23:19:12 +0530

    ADD: two functions related to encryption and decryption of strings

--- src/utils/others.js
+++ src/utils/others.js
@@ -1,3 +1,13 @@
+import { AES, enc } from "crypto-js";
+
 export function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function encryptString(string) {
+  return AES.encrypt(string, import.meta.env.VITE_SANITY_SECRET).toString();
+}
+
+export function decryptString(encryptedString) {
+  return AES.decrypt(encryptedString, import.meta.env.VITE_SANITY_SECRET).toString(enc.Utf8);
 }
