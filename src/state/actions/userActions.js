8354301ff9181e990ae43295f590f575532980ea
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-03-04 15:22:44 +0530

    MOD/ADD: checking users in DB before creating/logging one, using googleOAuth method and providing helpful feedback | created login action of user actionSet | added logic to provide users useful feedback while registering using register form

--- src/state/actions/userActions.js
+++ src/state/actions/userActions.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import { client } from "../../lib/sanityClient";
 
 import { formatUserInfo } from "../../utils";
-import { encryptString, sleep } from "../../utils/others";
+import { decryptString, encryptString, sleep } from "../../utils/others";
 
 export async function register(userDoc, dispatch) {
   try {
@@ -15,11 +15,13 @@ export async function register(userDoc, dispatch) {
 
     const existingUser = await client.fetch(query);
 
-    if (existingUser.length == 0) {
+    if (existingUser.length === 0) {
+      delete userDoc.isAgreed;
       const sanityUserDoc = {
         _type: "user",
         ...userDoc,
         password: encryptString(userDoc.password),
+        loginType: "manual",
       };
 
       const response = await client.create(sanityUserDoc);
@@ -34,11 +36,49 @@ export async function register(userDoc, dispatch) {
       localStorage.setItem("loginToken", encryptString(response._id));
 
       dispatch({ type: AT.USER_REGISTER_SUCCESS });
+    } else if (existingUser.length !== 0 && existingUser[0].loginType === "manual") {
+      throw new Error("User already exists with this email, please try logging in using login form");
     } else {
-      throw new Error("User already exists!");
+      throw new Error("User already exists with this email, please try logging in using google");
     }
   } catch (error) {
     dispatch({ type: AT.USER_REGISTER_FAIL });
+    throw error;
+  }
+}
+
+export async function login(userCred, dispatch) {
+  try {
+    dispatch({ type: AT.USER_LOGIN_REQUEST });
+
+    const query = `*[_type == "user" && email=="${userCred.email}"]`;
+
+    const existingUser = await client.fetch(query);
+
+    if (existingUser.length === 0) {
+      throw new Error("No user exist with this email");
+    } else if (existingUser.length !== 0 && existingUser[0].loginType === "gOAuth") {
+      throw new Error("Oops, you registered using Google, please continue with it");
+    }
+
+    const existingUserDecryptedPassword = decryptString(existingUser[0].password);
+
+    if (existingUserDecryptedPassword === userCred.password) {
+      dispatch({
+        type: AT.USER_DETAILS_LOGIN_TOKEN,
+        payload: { loginToken: existingUser[0]._id },
+      });
+
+      dispatch({ type: AT.USER_DETAILS_SUCCESS, payload: { info: existingUser[0] } });
+
+      localStorage.setItem("loginToken", encryptString(existingUser[0]._id));
+
+      dispatch({ type: AT.USER_LOGIN_SUCCESS });
+    } else {
+      throw new Error("Wrong password");
+    }
+  } catch (error) {
+    dispatch({ type: AT.USER_LOGIN_FAIL });
     throw error;
   }
 }
@@ -53,16 +93,27 @@ export async function oAuthLogin(accessToken, dispatch) {
 
     const { data: userInfo } = await axios.get(import.meta.env.VITE_GOOGLE_OAUTH_USERINFO_URL, config);
 
+    const query = `*[_type == "user" && email=="${userInfo.email}"]`;
+
+    const existingUser = await client.fetch(query);
+
+    let userDetails;
+
+    if (existingUser.length === 0) {
+      const userDoc = formatUserInfo(userInfo);
+      userDetails = await client.create(userDoc);
+    } else if (existingUser.length !== 0 && existingUser.loginType === "gOAuth") {
+      userDetails = existingUser[0];
+    } else {
+      throw new Error("User exist with this email, please try logging in using login form");
+    }
+
     dispatch({
       type: AT.USER_DETAILS_LOGIN_TOKEN,
-      payload: { loginToken: userInfo.sub },
+      payload: { loginToken: userDetails._id },
     });
 
-    localStorage.setItem("loginToken", encryptString(userInfo.sub));
-
-    const userDoc = formatUserInfo(userInfo);
-
-    const userDetails = await client.createIfNotExists(userDoc);
+    localStorage.setItem("loginToken", encryptString(userDetails._id));
 
     dispatch({ type: AT.USER_DETAILS_SUCCESS, payload: { info: userDetails } });
 
