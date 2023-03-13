d24fd6b5d09307b75142baf805d4dc220a5d3d25
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-03-13 14:07:24 +0530

    ADD : validation schema for profile details edit form

--- src/lib/yup/validationSchemas.js
+++ src/lib/yup/validationSchemas.js
@@ -4,13 +4,26 @@ const FILE_SIZE = 262144000;
 const SUPPORTED_FORMATS = ["image/jpg", "image/jpeg", "image/png"];
 
 export const investmentFormValSch = Yup.object({
-  investmentItem: Yup.string("Choose an investment item").required("This is required!"),
+  investmentItem: Yup.string("Choose an investment item").required(
+    "This is required!"
+  ),
   amount: Yup.number("Enter amount invested").required("This is required"),
   payment: Yup.string("Choose payment method").required("This is required"),
-  date: Yup.date().nullable().typeError("date is required").required("Date is required"),
+  date: Yup.date()
+    .nullable()
+    .typeError("date is required")
+    .required("Date is required"),
   receiptImg: Yup.mixed()
-    .test("fileSize", "File too large", (value) => value === null || value.size <= FILE_SIZE)
-    .test("fileFormat", "Unsupported file type", (value) => value === null || SUPPORTED_FORMATS.includes(value.type))
+    .test(
+      "fileSize",
+      "File too large",
+      (value) => value === null || value.size <= FILE_SIZE
+    )
+    .test(
+      "fileFormat",
+      "Unsupported file type",
+      (value) => value === null || SUPPORTED_FORMATS.includes(value.type)
+    )
     .notRequired(),
 });
 
@@ -18,11 +31,17 @@ export const saleFormValSch = Yup.object({
   saleItem: Yup.string("Choose a sale item").required("This is required"),
   unit: Yup.number("Enter number of units sold").required("This is required"),
   amount: Yup.number("Enter sale amount").required("This is required"),
-  date: Yup.date().nullable().typeError("date is required").required("Date is required"),
+  date: Yup.date()
+    .nullable()
+    .typeError("date is required")
+    .required("Date is required"),
 });
 
 export const historyFormValSch = Yup.object({
-  date: Yup.date().nullable().typeError("date is required").required("Date is required"),
+  date: Yup.date()
+    .nullable()
+    .typeError("date is required")
+    .required("Date is required"),
 });
 
 export const loginFormValSch = Yup.object({
@@ -36,4 +55,23 @@ export const registerFormValSch = Yup.object({
   email: Yup.string().email("Invalid email").required("Email is required"),
   password: Yup.string().required("Password is required"),
   isAgreed: Yup.boolean().oneOf([true], "Please agree before continuing"),
+});
+
+export const profileDetailsEditFormValSch = Yup.object({
+  profilePic: Yup.mixed()
+    .test(
+      "fileSize",
+      "File too large",
+      (value) => value === null || value.size <= FILE_SIZE
+    )
+    .test(
+      "fileFormat",
+      "Unsupported file type",
+      (value) => value === null || SUPPORTED_FORMATS.includes(value.type)
+    )
+    .notRequired(),
+  firstName: Yup.string().required("First Name is required").min(3).max(15),
+  lastName: Yup.string().required("Last Name is required").min(3).max(15),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string(),
 });
