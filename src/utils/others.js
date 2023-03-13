3590c7cc69c8fdac8a6fcb55e2d7192cf65370e1
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-03-13 13:45:08 +0530

    ADD: image resizing logic, object comparing and field filtering logic

--- src/utils/others.js
+++ src/utils/others.js
@@ -1,4 +1,5 @@
 import { AES, enc } from "crypto-js";
+import Resizer from "react-image-file-resizer";
 
 export function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -9,5 +10,33 @@ export function encryptString(string) {
 }
 
 export function decryptString(encryptedString) {
-  return AES.decrypt(encryptedString, import.meta.env.VITE_SANITY_SECRET).toString(enc.Utf8);
+  return AES.decrypt(
+    encryptedString,
+    import.meta.env.VITE_SANITY_SECRET
+  ).toString(enc.Utf8);
 }
+
+export function resizeFile(file) {
+  return new Promise((resolve) => {
+    Resizer.imageFileResizer(
+      file,
+      200,
+      200,
+      "JPEG",
+      100,
+      0,
+      (uri) => {
+        resolve(uri);
+      },
+      "file"
+    );
+  });
+}
+
+export const filterKeyValuePair = (obj1, obj2) => {
+  const filteredObj = Object.entries(obj1).filter(([key1, value1]) => {
+    return obj2[key1] !== value1;
+  });
+  const newObj = Object.fromEntries(filteredObj);
+  return newObj;
+};
