313da120d88ea4d5c2418abe704433837c5a30ac
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-03-31 22:56:48 +0530

    ADD : @rollup/plugin-alias to create path alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import alias from "@rollup/plugin-alias";
+
+import react from "@vitejs/plugin-react";
+
+import { defineConfig } from "vite";
+
+import { resolve } from "path";
+
+const projectRootDir = resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    alias({
+      entries: [
+        {
+          find: "@",
+          replacement: resolve(projectRootDir, "src"),
+        },
+      ],
+    }),
+  ],
+});
