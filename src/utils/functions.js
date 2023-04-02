2ab4902b5c657edc4b62322bf9f9d2e21e975bad
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-04-02 22:18:30 +0530

    MOD : moved combineReducer into utils function file

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -53,3 +53,19 @@ export function resizeFile(file) {
     );
   });
 }
+
+/**
+ * i This function is same as combineReducer from redux
+ * @param {Object} slices An Object containing all the reducers in a key-value pair
+ * @returns rootReducer which is a usual reducer but it remembers slices object as closure and operate upon it and not switch-case stuff
+ */
+export const combineReducers = (slices) => {
+  return (state, action) => {
+    return Object.keys(slices).reduce((accu, prop) => {
+      return {
+        ...accu,
+        [prop]: slices[prop](accu[prop], action),
+      };
+    }, state);
+  };
+};
